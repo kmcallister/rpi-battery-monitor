2a51ded3355855fc5e025fbd3a014df9af4f29f9
Keegan McAllister <mcallister.keegan@gmail.com>
2017-06-23 06:12:50 +0000

    Refactor sched

--- src/sched.rs
+++ src/sched.rs
@@ -4,6 +4,16 @@ use errno;
 /// Priority to use in realtime mode.
 const RT_PRIO: libc::c_int = 50;
 
+fn set_scheduler(policy: libc::c_int, prio: libc::c_int) {
+    let param = libc::sched_param {
+        sched_priority: prio,
+    };
+
+    unsafe {
+        assert_eq!(0, libc::sched_setscheduler(0, policy, &param));
+    }
+}
+
 /// RAII helper for entering/exiting realtime scheduler priority.
 pub struct Realtime {
     orig_policy: libc::c_int,
@@ -27,13 +37,7 @@ impl Realtime {
         };
         assert_eq!(0, errno::errno().0);
 
-        let param = libc::sched_param {
-            sched_priority: RT_PRIO,
-        };
-
-        unsafe {
-            assert_eq!(0, libc::sched_setscheduler(0, libc::SCHED_FIFO, &param));
-        }
+        set_scheduler(libc::SCHED_FIFO, RT_PRIO);
 
         Realtime {
             orig_policy: policy,
@@ -44,12 +48,6 @@ impl Realtime {
 
 impl Drop for Realtime {
     fn drop(&mut self) {
-        let param = libc::sched_param {
-            sched_priority: self.orig_prio,
-        };
-
-        unsafe {
-            assert_eq!(0, libc::sched_setscheduler(0, self.orig_policy, &param));
-        }
+        set_scheduler(self.orig_policy, self.orig_prio);
     }
 }
